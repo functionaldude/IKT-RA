7315d7d432f695814a4ff07f52fa8fd2877faa44
Zoltán Sasvári <z.sasvari@student.tugraz.at>
2015-12-04 16:02:41 +0100

    added rpi libs

--- source/init.s
+++ source/init.s
@@ -4,6 +4,31 @@
 
 _start:
 	bl main @first part of main
+
+hang: b hang
+
+.globl PUT32
+PUT32:
+    str r1,[r0]
+    bx lr
+
+.globl GET32
+GET32:
+    ldr r0,[r0]
+    bx lr
+
+.globl dummy
+dummy:
+    bx lr
+
+.globl GETPC
+GETPC:
+    mov r0,lr
+    bx lr
+
+.globl BRANCHTO
+BRANCHTO:
+    bx r0
 		
 .section .text @will hold code
 		
